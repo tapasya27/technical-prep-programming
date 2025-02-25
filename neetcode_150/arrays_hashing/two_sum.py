0824df9e2f6c1c89276c0c7aafcf55be7e283f8d
Tapasya Sharma <t.sharm7377@gmail.com>
2025-02-24 23:33:24 -0800

    Two sum part 1

---
+++ neetcode_150/arrays_hashing/two_sum.py
@@ -0,0 +1,25 @@
+﻿"""
+Problem:
+Difficulty: Easy 
+"""
+
+from typing import List
+class Solution:
+	"""
+	Solution: 
+	Space Complexity: O(n)
+	Time Complexity: O(n)
+	"""
+	class Solution:
+		def twoSum(self, nums: List[int], target: int) -> List[int]:
+			num_map = {}
+			for i, num in enumerate(nums):
+				complement = target - num
+				if complement in num_map:
+					return [num_map[complement], i]
+				num_map[num] = i  # Store current number's index
+			return [] 
+
+solution = Solution()
+result = solution.twosum([3,4,5,6],7) # returns [0,1]
+print(result)
